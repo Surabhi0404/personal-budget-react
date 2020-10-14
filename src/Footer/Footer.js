d4ba0233b49fbb07078789863e0c6fb025cf9e1c
Surabhi0404 <surabhigurav96@gmail.com>
2020-10-14 16:57:55 -0400

    Brought PBudget code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bottom" role="footer">
+            <div className="center">
+                All rights reserved &copy; <cite>Fabio Nolasco</cite>
+            </div>
+    </footer>
+  );
+}
+
+export default Footer;
